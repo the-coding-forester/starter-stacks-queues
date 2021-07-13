83473a491834328958cf8c025fdcc74482f63ff2
Stephanie Moore <smoore_forestry@outlook.com>
2021-07-13 18:21:53 -0400

    added enqueue method to stack class

--- src/Queue.js
+++ src/Queue.js
@@ -12,4 +12,18 @@ class Queue {
     this.first = null;
     this.last = null;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (this.first) {
+      this.last.next = newNode;
+    } else {
+      // Set the node of the queue's next pointer to be the new node
+      this.first = newNode;
+    }
+
+    // Make the new node the last item on the queue
+    this.last = newNode;
+  }
 }
